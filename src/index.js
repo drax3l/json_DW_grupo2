a4012d050c553ce558224ee4c8783c0048677fde
drax3l <alexjeanpierquispesierra@gmail.com>
2025-01-31 06:00:02 -0500

    realizar mejoras

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+
 import router from './routes/index.js';
 import {conectar} from './services/conexion.js';
 
@@ -9,6 +11,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(__dirname, 'public')));
 conectar();
+app.use(cors());
+const corsOptions = {
+    origin: 'https://json-dw-grupo2-1.onrender.com',
+    optionsSuccessStatus: 200
+}
 
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
